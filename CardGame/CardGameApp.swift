806987179a32900ff95391a4d1c32f812c6814a6
Arslan <jacklol5760@gmail.com>
2023-06-19 16:03:20 +0600

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Arslan Toimbekov on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
